efc18c80b7c5249703b589c2b85888468c0d9537
Hana Lee <hanalee07@gmail.com>
2021-12-28 14:12:07 -0600

    Sets up basic index route for server app

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,12 @@ WORKDIR /app
 
 COPY Cargo.toml .
 COPY Cargo.lock .
-COPY src* ./src
 COPY migrations* ./migrations
 COPY diesel.toml . 
-
 RUN cargo install diesel_cli --no-default-features --features postgres 
+
+COPY src* ./src
+COPY Rocket.toml .
 RUN cargo build
 
 CMD diesel migration run && cargo run
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::response::status;
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> status::Accepted<String> {
+    status::Accepted(Some(String::from("Hello, world!")))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
