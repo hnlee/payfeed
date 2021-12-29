c82d975bcd3e33fff1d39e11a6bb95c8f8dc7f4d
Hana Lee <hanalee07@gmail.com>
2021-12-29 16:31:34 -0600

    Still not working

--- src/main.rs
+++ src/main.rs
@@ -9,52 +9,7 @@ use std::env;
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    //     let matches = App::new("consumer example")
-    //         .version(option_env!("CARGO_PKG_VERSION").unwrap_or(""))
-    //         .about("Simple command line consumer")
-    //         .arg(
-    //             Arg::with_name("brokers")
-    //                 .short("b")
-    //                 .long("brokers")
-    //                 .help("Broker list in kafka format")
-    //                 .takes_value(true)
-    //                 .default_value("localhost:9092"),
-    //         )
-    //         .arg(
-    //             Arg::with_name("group-id")
-    //                 .short("g")
-    //                 .long("group-id")
-    //                 .help("Consumer group id")
-    //                 .takes_value(true)
-    //                 .default_value("example_consumer_group_id"),
-    //         )
-    //         .arg(
-    //             Arg::with_name("log-conf")
-    //                 .long("log-conf")
-    //                 .help("Configure the logging format (example: 'rdkafka=trace')")
-    //                 .takes_value(true),
-    //         )
-    //         .arg(
-    //             Arg::with_name("topics")
-    //                 .short("t")
-    //                 .long("topics")
-    //                 .help("Topic list")
-    //                 .takes_value(true)
-    //                 .multiple(true)
-    //                 .required(true),
-    //         )
-    //         .get_matches();
-
-    //     setup_logger(true, matches.value_of("log-conf"));
-
-    //     let (version_n, version_s) = get_rdkafka_version();
-    //     info!("rd_kafka_version: 0x{:08x}, {}", version_n, version_s);
-    // let topics = matches.values_of("topics").unwrap().collect::<Vec<&str>>();
-    // let brokers = matches.value_of("brokers").unwrap();
-    // let group_id = matches.value_of("group-id").unwrap();
-
-    // consume_and_print(brokers, group_id, &topics).await;
-
+    payfeed::run_consumer()
     HttpResponse::Ok().body("Hello world!")
 }
 
