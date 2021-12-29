ee918387d2552c658ca338fb207de59e77ef687d
Hana Lee <hanalee07@gmail.com>
2021-12-29 14:05:44 -0600

    Create a new user when requesting POST /users

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,39 @@
-// this struct will use to receive user input
-// #[derive(Serialize, Deserialize)]
-// pub struct TodoRequest {
-//     pub description: String,
-//         pub done: bool,
-//         }
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use sqlx::postgres::PgPool;
+use sqlx::FromRow;
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct NewUser {
+    pub name: String,
+}
+
+#[derive(Serialize, FromRow, Debug)]
+pub struct User {
+    pub id: Uuid,
+    pub name: String,
+}
+
+impl User {
+    pub async fn create(new_user: NewUser, pool: &PgPool) -> Result<User> {
+        let mut transaction = pool.begin().await?;
+        let record: User = sqlx::query_as(
+            "
+            INSERT INTO users (name)
+            VALUES ($1)
+            RETURNING id, name
+            ",
+        )
+        .bind(new_user.name)
+        .fetch_one(&mut transaction)
+        .await?;
+
+        transaction.commit().await?;
+
+        Ok(User {
+            id: record.id,
+            name: record.name,
+        })
+    }
+}
